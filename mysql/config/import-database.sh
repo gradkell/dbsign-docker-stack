9dd19c478382b448cf109963378d9cb28fb9a8d7
Cole Barnes <cbarnes@gradkell.com>
2020-12-16 10:03:15 -0600

    Fixed SQL import script

--- mysql/config/import-database.sh
+++ mysql/config/import-database.sh
@@ -4,4 +4,4 @@
 # docker exec -it `docker container ls | grep dbsign_mysql | cut -f1 -d " "` /opt/dbsign/import-database.sh
 
 echo importing database...
-mysql -u dbsign -p Qwerty_123 dbsign < /opt/dbsign/dbsign.sql
+mysql -u dbsign -p dbsign < /opt/dbsign/dbsign.sql
