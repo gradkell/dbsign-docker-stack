db81f10aa19b334c25f516d3d081bfd7ba38c416
Cole Barnes <cbarnes@gradkell.com>
2021-05-17 16:42:48 -0500

    Added framework for httpd/ssl volume

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@
 cp -r ./tomcat/dbsign/* ./volumes/dbsign/
 mkdir ./volumes/dbsign/logs
 rm ./volumes/database/README.md
+cp -r ./httpd/ssl/* ./volumes/ssl
 
 docker-compose -p "dbsign" build --no-cache
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
