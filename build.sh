f89f0c24e3f63621cacd3aabb0987c87ecaedb8a
Cole Barnes <cbarnes@gradkell.com>
2021-05-17 16:36:34 -0500

    Updated build script

--- build.sh
+++ build.sh
@@ -2,6 +2,7 @@
 
 cp -r ./tomcat/dbsign/* ./volumes/dbsign/
 mkdir ./volumes/dbsign/logs
+rm ./volumes/database/README.md
 
 docker-compose -p "dbsign" build --no-cache
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
