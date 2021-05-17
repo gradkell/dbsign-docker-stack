c7831e22f8ff725c6fa266893877ee3b4ae743d2
Cole Barnes <cbarnes@gradkell.com>
2021-05-17 16:56:41 -0500

    Moved ssl certs/keys to a volume.

--- build.sh
+++ build.sh
@@ -5,5 +5,8 @@ mkdir ./volumes/dbsign/logs
 rm ./volumes/database/README.md
 cp -r ./httpd/ssl/* ./volumes/ssl
 
+wget https://demo.dbsign.com/pkiccu-data/bundles/SSLCADNRequestFile -O ./volumes/ssl/pki/SSLCADNRequestFile
+wget https://demo.dbsign.com/pkiccu-data/bundles/SSLCACertificateFile -O ./volumes/ssl/pki/SSLCACertificateFile
+
 docker-compose -p "dbsign" build --no-cache
 docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
